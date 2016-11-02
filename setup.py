f7910bf9000808b402c46b5515c54c76f7acc002
Tyler Brough <broughtj@users.noreply.github.com>
2016-11-02 15:07:52 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 setup(name='probo', version='0.1', packages=['probo'], author='Tyler J. Brough',
         author_email = "tyler.brough@usu.edu", description="The Probo Option Pricing Module",
-        keywords = "option pricing object-oriented design patterns Monte Carlo Binomial Black-Schols",
+        keywords = "option pricing object-oriented design patterns Monte Carlo Binomial Black-Scholes",
         url="https://github.com/broughtj/probo")
